c0c5226e82221b7a616d6c0623ed48aef1b31c4f
hymndrazill <srihafiras@yahoo.fr>
2022-11-18 01:01:38 +0100

    use states, button submit func and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import React, { useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
-
+import {Todo } from "../src/model"
 const App:React.FC = ()=> {
+  const [todo, setTodo] = useState<string>('');
+    const [todos, setTodos] = useState<Todo[]>([])
 
+    const handleAdd = (e: React.FormEvent) =>{
+      e.preventDefault();
+      if(todos.length){
+        setTodos([...todos, {id: Date.now(), todo:todo, isDone:false}]);
+
+      }
+    }
   return (
     <div className="App">
         <span className='heading'>Tasks</span>
-       <InputField/>
+       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
       
   )
