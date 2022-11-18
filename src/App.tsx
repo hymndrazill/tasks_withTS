22f949172fe0a0a2134d2766f3ceb470c0a6aeba
hymndrazill <srihafiras@yahoo.fr>
2022-11-18 17:48:21 +0100

    added props

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import InputField from './components/InputField'
 import {Todo } from "../src/model"
+import TodoList from './components/TodoList'
 const App:React.FC = ()=> {
   const [todo, setTodo] = useState<string>('');
     const [todos, setTodos] = useState<Todo[]>([])
 
     const handleAdd = (e: React.FormEvent) =>{
       e.preventDefault();
-      if(todos.length){
-        setTodos([...todos, {id: Date.now(), todo:todo, isDone:false}]);
-
+      if(todo){
+        setTodos([...todos, { id: Date.now(), todo, isDone:false }]);
+        setTodo('');
       }
     }
+  
+
   return (
     <div className="App">
         <span className='heading'>Tasks</span>
        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList setTodos={setTodos} todos={todos}/>     
     </div>
       
   )
