45214f2263a0fa202a62ab2c0f5fd2f49a3e7bb1
hymndrazill <srihafiras@yahoo.fr>
2022-11-18 01:00:53 +0100

    styling and adding the eventListeners in comp

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,23 @@
 import "./inputfield.css"
 
-const InputField = () => {
+interface Props {
+    todo:string;
+    setTodo:React.Dispatch<React.SetStateAction<string>>;
+    handleAdd:  (e: React.FormEvent)=>void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo,handleAdd}) => {
   return (
     <form className='input'>
-        <input className='input__box' type="input" placeholder='Write down your task.' />
-        <button className='input__submit' type="submit">Add Task</button>
+        <input className='input__box' type="input" placeholder='Write down your task.'
+        value={todo}
+        onChange={(e)=>setTodo(e.target.value)}
+        />
+        <button 
+        className='input__submit' 
+        onSubmit={handleAdd}
+        type="submit">Add Task
+        </button>
     </form>
   )
 }
